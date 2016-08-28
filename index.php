85c40592a922e08cc0db8dcdecd15f32624e2afe
6148nathalie <tsanceo@yahoo.com>
2016-08-28 14:03:39 +0200

    ajout php - envoi de mail ok

--- index.php
+++ index.php
@@ -1,24 +1,65 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css" media="print">
-    <title>Envoi de mail</title>
-</head>
-<body>
-    <form method="post">
-    <div>
-        <label for="nom">Nom :</label>
-        <input type="text" id="nom" />
-    </div>
-    <div>
-        <label for="courriel">Courriel :</label>
-        <input type="email" id="courriel" />
-    </div>
-    <div>
-        <label for="message">Message :</label>
-        <textarea id="message"></textarea>
-    </div>
-</form>
-</body>
+<?php
+if(isset($_POST['mailform']))
+{
+
+if (!empty($_POST['nom']) AND !empty($_POST['mail']) AND ! empty($_POST['message']))
+{
+$header="MIME-Version: 1.0\r\n";
+$header.='From:"Thomas"<foch29310@gmail.com>'."\n";
+$header.='Content-Type:text/html; charset="uft-8"'."\n";
+$header.='Content-Transfer-Encoding: 8bit';
+
+$message='
+<html>
+	<body>
+		<div align="center">
+					<br />
+					<u>Nom de l\'expéditeur :</u>'.$_POST['nom'].'<br />
+					<u>Mail de l\'expéditeur :</u>'.$_POST['mail'].'<br />
+					<br />
+					'.nl2br($_POST['message']).'
+					<br />
+		</div>
+	</body>
 </html>
+';
+
+mail("foch29310@gmail.com", "CONTACT - SITE", $message, $header);
+    $msg="Votre message a bien été envoyé !";
+}
+    else {
+        $msg ="Tous les champs doivent être complétés";
+    }
+}
+?>
+    <html>
+
+    <head>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+        <h2>Formulaire de contact !</h2>
+        <form method="POST" action="">
+            <input type="text" name="nom" placeholder="Votre nom" value="<?php if(isset($_POST['nom'])) { echo $_POST['nom']; } ?>" />
+            <br />
+            <br />
+            <input type="email" name="mail" placeholder="Votre email" value="<?php if(isset($_POST['mail'])) { echo $_POST['mail']; } ?>" />
+            <br />
+            <br />
+            <textarea name="message" placeholder="Votre message">
+                <?php if(isset($_POST['message'])) { echo $_POST['message']; } ?>
+            </textarea>
+            <br />
+            <br />
+            <input type="submit" value="Envoyer !" name="mailform" />
+        </form>
+        <?php
+		if(isset($msg))
+		{
+			echo $msg;
+		}
+		?>
+    </body>
+
+    </html>
