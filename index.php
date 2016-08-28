ccd765eea4e4b6a25f7f83835ae208479548dd73
6148nathalie <tsanceo@yahoo.com>
2016-08-28 10:31:47 +0200

    ajout html

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css" media="print">
+    <title>Envoi de mail</title>
+</head>
+<body>
+    <form method="post">
+    <div>
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" />
+    </div>
+    <div>
+        <label for="courriel">Courriel :</label>
+        <input type="email" id="courriel" />
+    </div>
+    <div>
+        <label for="message">Message :</label>
+        <textarea id="message"></textarea>
+    </div>
+</form>
+</body>
+</html>
