b780ff5fa2bd4f6fd2d7b4cdadbe5782d532b436
6148nathalie <tsanceo@yahoo.com>
2016-08-30 23:37:49 +0200

    ajout css + nouvelle fonctionnalité selection objet qui fonctionne pas

--- index.php
+++ index.php
@@ -1,13 +1,63 @@
-<?php
-if(isset($_POST['mailform']))
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <div class="container-fluid">
+
+        <div class="absolute background"></div>
+        <div class="absolute filtre"></div>
+        <h3 class="police-3 text-center">Thomas Sanceo</h3>
+        <h2 class="police-2 text-center">Contact me</h2>
+        <form class="absolute centrage text-center" method="POST" action="">
+            <input type="text" class="form-control input-lg" name="nom" placeholder="Votre nom" value="<?php if(isset($_POST['nom'])) { echo $_POST['nom']; } ?>" />
+            <br />
+            <br />
+            <input type="email" class="form-control input-lg" name="mail" placeholder="Votre email" value="<?php if(isset($_POST['mail'])) { echo $_POST['mail']; } ?>" />
+            <br />
+            <label for="sel2"></label>
+            <select class="form-control input-lg" name="objet">
+                <option value="1"  <?php if ($_POST['objet']=='1') {echo "selected='selected'";}?>>Versement d'argent</option>
+                <option value="2" <?php if ($_POST['objet']=='2') {echo "selected='selected'";}?>>Demande adresse paypal</option>
+                <option value="3" <?php if ($_POST['objet']=='3') {echo "selected='selected'";}?>>Virement bancaire</option>
+                <option value="4" <?php if ($_POST['objet']=='4') {echo "selected='selected'";}?>>Versement bitcoin</option>
+            </select>
+            <br />
+            <br />
+            <textarea class="form-control" name="message" placeholder="Votre message" cols="175" rows="18">
+
+            </textarea>
+            <br />
+            <br />
+            <input type="submit" class="btn btn-primary btn-lg" value="Envoyer !" name="envoi" />
+        </form>
+    </div>
+
+
+    <?php
+session_start();
+        
+if(isset($_POST['envoi']))
 {
 
 if (!empty($_POST['nom']) AND !empty($_POST['mail']) AND ! empty($_POST['message']))
 {
 $header="MIME-Version: 1.0\r\n";
-$header.='From:"Thomas"<foch29310@gmail.com>'."\n";
-$header.='Content-Type:text/html; charset="uft-8"'."\n";
+$header.='From:"Thomas"<totodu29310@gmail.com>'."\n";
+$header.='Content-Type:multipart/mixed; charset="uft-8"'."\n";
 $header.='Content-Transfer-Encoding: 8bit';
+// $objet = ($_POST['objet']); fonctionne pas
+
 
 $message='
 <html>
@@ -24,42 +74,19 @@ $message='
 </html>
 ';
 
-mail("foch29310@gmail.com", "CONTACT - SITE", $message, $header);
+mail("totodu29310@gmail.com", "CONTACT - SITE", $message, $header);
     $msg="Votre message a bien été envoyé !";
 }
     else {
         $msg ="Tous les champs doivent être complétés";
     }
+
 }
-?>
-    <html>
-
-    <head>
-        <meta charset="utf-8">
-    </head>
-
-    <body>
-        <h2>Formulaire de contact !</h2>
-        <form method="POST" action="">
-            <input type="text" name="nom" placeholder="Votre nom" value="<?php if(isset($_POST['nom'])) { echo $_POST['nom']; } ?>" />
-            <br />
-            <br />
-            <input type="email" name="mail" placeholder="Votre email" value="<?php if(isset($_POST['mail'])) { echo $_POST['mail']; } ?>" />
-            <br />
-            <br />
-            <textarea name="message" placeholder="Votre message">
-                <?php if(isset($_POST['message'])) { echo $_POST['message']; } ?>
-            </textarea>
-            <br />
-            <br />
-            <input type="submit" value="Envoyer !" name="mailform" />
-        </form>
-        <?php
-		if(isset($msg))
-		{
+        
+if(isset($msg)){
 			echo $msg;
 		}
 		?>
-    </body>
+</body>
 
-    </html>
+</html>
